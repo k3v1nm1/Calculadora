f30a7ffd3baa570944faf38cbe0d880e2f700ce5
kevin.mirandahe@amigo.edu.co <you@example.com>
2022-10-10 09:28:44 -0700

    Primer commit de la calculadora

---
+++ Index.js
@@ -0,0 +1,24 @@
+function multiplicar(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+   document.getElementById("respuesta").innerHTML= num1 * num2;
+
+}
+
+function dividir(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+   document.getElementById("respuesta").innerHTML= num1 / num2;
+
+}
+
+function restar(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+   document.getElementById("respuesta").innerHTML= num1 - num2;
+
+}
+
